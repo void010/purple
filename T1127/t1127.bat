40f9214f1c4840ad27930a94b396d3705d6b8c30
void010 <50961936+void010@users.noreply.github.com>
2021-01-10 10:01:42 -0500

    Add files via upload

---
+++ T1127/t1127.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo Starting Execution
+
+:: EDIT THE PATH OF THE csproj FILE
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe C:\Users\Alfred\Desktop\purple\Test_2\T1127.001.csproj
+
+pause
