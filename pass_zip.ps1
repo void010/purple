4fa2fceacff3ac646d321753ef3f0b21c8afb368
void010 <50961936+void010@users.noreply.github.com>
2021-01-12 08:49:16 -0500

    Add files via upload

---
+++ pass_zip.ps1
@@ -0,0 +1,35 @@
+function Write-ZipUsing7Zip([string]$FilesToZip, [string]$ZipOutputFilePath, [string]$Password, [ValidateSet('7z','zip','gzip','bzip2','tar','iso','udf')][string]$CompressionType = 'zip', [switch]$HideWindow)
+{
+    # Look for the 7zip executable.
+    $pathTo32Bit7Zip = "C:\Program Files (x86)\7-Zip\7z.exe"
+    $pathTo64Bit7Zip = "C:\Program Files\7-Zip\7z.exe"
+    $THIS_SCRIPTS_DIRECTORY = Split-Path $script:MyInvocation.MyCommand.Path
+    $pathToStandAloneExe = Join-Path $THIS_SCRIPTS_DIRECTORY "7za.exe"
+    if (Test-Path $pathTo64Bit7Zip) { $pathTo7ZipExe = $pathTo64Bit7Zip }
+    elseif (Test-Path $pathTo32Bit7Zip) { $pathTo7ZipExe = $pathTo32Bit7Zip }
+    elseif (Test-Path $pathToStandAloneExe) { $pathTo7ZipExe = $pathToStandAloneExe }
+    else { throw "Could not find the 7-zip executable." }
+
+    # Delete the destination zip file if it already exists (i.e. overwrite it).
+    if (Test-Path $ZipOutputFilePath) { Remove-Item $ZipOutputFilePath -Force }
+
+    $windowStyle = "Normal"
+    if ($HideWindow) { $windowStyle = "Hidden" }
+
+    # Create the arguments to use to zip up the files.
+    # Command-line argument syntax can be found at: http://www.dotnetperls.com/7-zip-examples
+    $arguments = "a -t$CompressionType ""$ZipOutputFilePath"" ""$FilesToZip"" -mx9"
+    if (!([string]::IsNullOrEmpty($Password))) { $arguments += " -p$Password" }
+
+    # Zip up the files.
+    $p = Start-Process $pathTo7ZipExe -ArgumentList $arguments -Wait -PassThru -WindowStyle $windowStyle
+
+    # If the files were not zipped successfully.
+    if (!(($p.HasExited -eq $true) -and ($p.ExitCode -eq 0)))
+    {
+        throw "There was a problem creating the zip file '$ZipFilePath'."
+    }
+}
+
+Write-ZipUsing7Zip -FilesToZip "C:\Users\alfred.v\Desktop\Test_2" -ZipOutputFilePath "C:\Users\alfred.v\Desktop\folder.zip" -Password "password123"
+
