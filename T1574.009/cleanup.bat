6f8a47d287865a21bf781cfe38bdc81882606776
void010 <50961936+void010@users.noreply.github.com>
2021-03-17 15:54:42 +0530

    Update cleanup.bat

--- T1574.009/cleanup.bat
+++ T1574.009/cleanup.bat
@@ -5,3 +5,4 @@ sc delete "Example Service" >nul 2>&1
 del "C:\Program Files\program.exe" >nul 2>&1
 del "C:\program.exe" >nul 2>&1
 del "C:\Temp\download.txt" >nul 2>&1
+del "C:\users\alfred.v\Downloads\cpp_poc_64.exe" >nul 2>&1
