a2c8309d225cff1c76f84d7b2600dc2dcb70495a
void010 <50961936+void010@users.noreply.github.com>
2021-04-28 15:59:53 +0530

    Update zip.py

--- Archive via library/zip.py
+++ Archive via library/zip.py
@@ -5,3 +5,13 @@ test_zip.write('C:\\Temp\\1.txt', compress_type=zipfile.ZIP_DEFLATED)
 test_zip.write('C:\\Temp\\2.txt', compress_type=zipfile.ZIP_DEFLATED)
 
 test_zip.close()
+
+
+import shutil
+import os
+
+entries = os.listdir('C:\\Temp')
+for entry in entries:
+	print(entry)
+	
+shutil.make_archive('C:\\Temp\\zipper_test', 'zip', 'C:\\Temp')
